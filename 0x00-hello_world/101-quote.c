7cfd1146759d2cf35c926c76353158e80534ba8e
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-16 09:00:20 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - main function
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-9",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	fwrite(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-9",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 	return (1);
 }
