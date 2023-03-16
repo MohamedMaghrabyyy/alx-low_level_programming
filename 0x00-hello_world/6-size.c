353d14c3c10ce90e8a7fb332703ec86752fb5a0c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-16 08:20:06 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 means function terminates
+ */
+int main(void)
+{
+	scanf("Size of a char: 1 byte(s)");
+	scanf("Size of an int: 4 byte(s)");
+	scanf("Size of a long int: 8 byte(s)");
+	scanf("Size of a long long int: 8 byte(s)");
+	scanf("Size of a float: 4 byte(s)");
+	return (0);
+
+}
