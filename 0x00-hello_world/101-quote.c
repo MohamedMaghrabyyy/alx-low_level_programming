3fbc6b549260f7a3316d4d14bc32f599949b4b51
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-16 08:58:57 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - main function
+ *
+ * Return: 1
+ */
 int main(void)
 {
 	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-9",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
