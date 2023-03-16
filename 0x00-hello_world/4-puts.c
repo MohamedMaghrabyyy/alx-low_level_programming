e7a015e13a2779ad5cb9b2163a193157106846ae
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-16 08:09:59 -0700

    Task 4:done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * @str[] - an array of characters
+ *
+ * Return: Terminates the main function
+ */
+
+int main(void)
+{
+	char str[]="Programming is like building a multilingual puzzle\n";
+	puts(str);
+	return 0;
+}
+
