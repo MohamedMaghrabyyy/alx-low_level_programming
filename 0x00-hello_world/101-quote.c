50892113170adb1359101e5a148c8868c00be2f7
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-16 08:56:58 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-9",strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
+	return (1);
+}
