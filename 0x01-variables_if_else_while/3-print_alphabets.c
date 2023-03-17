a7a9b0d758590556d8989a1ab6983b9c7f05ca29
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 13:42:15 -0700

    Task 3 : done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-        char c;
-
-        for (c = 97; c <= 122; c++)
-                putchar(c);
-        for (c = 65; c <= 90; c++)
+	char c;
+	for (c = 97; c <= 122; c++)
+		putchar(c);
+	for (c = 65; c <= 90; c++)
 		putchar(c);
 	putchar('\n');
-
-        return (0);
+	
+	return (0);
 }
