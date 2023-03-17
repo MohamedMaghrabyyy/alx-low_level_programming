1177a42b51a4a5bd0bae29c9be7da0aa66cd9890
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 14:51:08 -0700

    Task 10 : done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+	int n;
+	int m;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for(n = c+1; n <= 57; n++)
+		{
+			for(m = n+1; m <=57; m++)
+			{
+				putchar(c);
+				putchar(n);
+				putchar(m);
+				if(c + n + m != 168)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
