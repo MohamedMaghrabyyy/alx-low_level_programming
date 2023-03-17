3e251355e6618ef268abc7a7a4755089fd04679e
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 13:22:54 -0700

    Task 2: done

---
+++ 0x01-variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	for(c = 97 ; c <= 122; c++ )
+	       	putchar(c);
+    
+
+	return (0);
+}
