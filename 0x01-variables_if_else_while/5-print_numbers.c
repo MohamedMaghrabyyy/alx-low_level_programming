901ecb6442003484fced47f7573ceb6a5809ee32
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 13:56:47 -0700

    Task 5 : done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+	for (c = 0; c <= 9; c++)
+		printf("%d",c);
+	putchar('\n');
+	return (0);
+
+}
