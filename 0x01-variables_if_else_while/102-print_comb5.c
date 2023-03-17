f2a8e0d8367ec00bf0abb449cb3d48ca20a0c2f9
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 15:27:49 -0700

    Final task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success(
+ */
+int main(void)
+{
+	int p;
+	int q;
+	for (p = 0;p <= 99; p++)
+	{
+		for (q=0 ; q <= 98; q++)
+		{
+			putchar((p/10)+'0');
+			putchar((p%10)+'0');
+			putchar(' ');
+			if (p == 99 && q == 98)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+	putchar(' ');
+	return (0);
+}
