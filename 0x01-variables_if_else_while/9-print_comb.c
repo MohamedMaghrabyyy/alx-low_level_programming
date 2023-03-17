185786b63453833df9a759372881653468212ca5
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 14:30:22 -0700

    Task 9 : done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(36);
+	return (0);
+}
