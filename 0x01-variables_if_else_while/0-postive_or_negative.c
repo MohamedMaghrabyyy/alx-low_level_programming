f213ba6d3e5e16883d48251064a0cffeebbb030a
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 11:08:58 -0700

    Task 0: done

--- 0x01-variables_if_else_while/0-postive_or_negative.c
+++ 0x01-variables_if_else_while/0-postive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
- * main - Entry point
- * @n - randomized value
- *
- * if - checks if number is +ve
- * else if - checks if number equals 0
- * else - number is negative
- * Return: 0 (success)
 
-/* betty style doc for function main goes there */
+/*
+ * main - Entry point
+ * @n - Randomized value
+ *
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
 	int n;
@@ -18,11 +16,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is postive", n);
-	else if(n == 0)
-		printf("%d is zero", n);
-	else
-		printf("%d is negative", n);
+    {
+        printf("%d is postive",n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative",n);
+    }
+    else
+    {
+        printf("%d is zero",n);
 
+    }
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-Task 0: done
+Readme file created
