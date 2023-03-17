c302a05c7da20d2c90539908e1514895b0b3896b
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-17 11:20:56 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,34 +4,28 @@
 
 /*
  * main - Entry point
- * @n - Randomized value
  *
  * Return: 0 (success)
  */
 
 int main(void)
 {
-	int n;
+    int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-	       	printf("%d is postive", n);
-       	}
-
-
-   	 else if (n < 0)
-    	 {
-		 printf("%d is negative", n);
-   	 }
-   	
-       	else
-   	{
-	       	printf("%d is zero", n);
-
-    	}
-	
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative\n", n);
+    }
+    else
+    {
+        printf("%d is zero\n", n);
+    }
 	return (0);
 }
 
