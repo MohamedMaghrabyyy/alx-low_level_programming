b7af7a1310bab640e23677ad58f32df702594936
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 15:09:17 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,22 +10,22 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while(n >= 98)
+		while (n >= 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n = n - 1;
 		}
 	}
-	else if (n =< 98)
+	else if (n <= 98)
 	{
-		while(n < 98)
+		while (n < 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n = n + 1;
 		}
 	}
 	else
 	{
-		printf("%d",98);
+		printf("%d", 98);
 	}
 }
