619136c5e98b6ba792cab1f35d228ec0fa1e6087
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 14:45:31 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,12 @@
 /**
  * add - adds two numbers
  * @num1: first num
- * @num2L second num
+ * @num2: second num
  *
  * Return: sum of two numbers
  */
 
 int add(int num1, int num2)
 {
-	return (num1+num2);
+	return (num1 + num2);
 }
