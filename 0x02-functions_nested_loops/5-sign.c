ea35a9194360ea17ee8d598af30400a230268432
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 13:48:55 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - checks for sign of number
+ * @n: number to be checked
+ *
+ * Return: 1 if postive
+ * 0 if zero
+ * -1 if negative
+ */
+
+int print_sign(int n)
+{
+	int i;
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else if (i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
