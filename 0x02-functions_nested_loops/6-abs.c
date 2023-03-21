f888eea7638fb2998ea6e7d25c291c121fa4a9f1
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 14:02:06 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - returns absolute value of a number
+ * @n: number for its absolute value to be found
+ *
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	return (n*n)/n;
+
+}
