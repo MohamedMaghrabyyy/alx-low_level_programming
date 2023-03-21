0eea63f83bd7e0fc178f0f64499f5b172e4351ea
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 14:08:00 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: number to get last digit from
+ *
+ * Return: last digit is returned
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
