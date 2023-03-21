b17c2adeb5353dca301e2b858621ee5addab9b24
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 15:44:45 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * times_table - print times table from zero to nine
+ *
+ * Return: print times table
+ */
+void times_table(void)
+{
+	int j;
+	int k;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0 ; j <= 9; j++)
+		{
+			_putchar(j + '0');
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
