596ff9a33d3dc505117cfcec79991b621a70c0fe
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-22 15:52:19 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,22 +10,24 @@ int main(void)
 	long arr[100] = {0};
 	int i;
 	long sum;
+	int count;
 
 	arr[0] = 1;
 	arr[1] = 2;
 
 	for (i = 2; i <= 100; i++)
 	{
-		if (arr[i-1] <= 4000000)
+		if (arr[i - 1] <= 4000000)
 		{
 			arr[i] = arr[i - 1] + arr[i - 2];
+			count++:
 		}
 		else
 		{
 			break;
 		}
 	}
-	for (i = 0; i<= 100; i++)
+	for (i = 0; i <= count; i++)
 	{
 		if (arr[i] != 0 && arr[i] % 2 == 0)
 		{
