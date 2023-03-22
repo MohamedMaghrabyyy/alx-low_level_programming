e5dc74d49d0807518145b7e26b13a20c805c0198
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-22 15:35:24 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int arr[50]={1, 2};
+	long long arr[50] = {1, 2};
 	int i;
 
 	for (i = 2; i <= 49 ; i++)
 	{
 		arr[i] = arr[i - 2] +  arr[i - 1];
 	}
-	for (i = 1; i <= 48; i++)
+	for (i = 0; i <= 48; i++)
 	{
-		printf("%d, ", arr[i]);
+		printf("%ld, ", arr[i]);
 	}
-	printf("%d", arr[49]);
+	printf("%ld", arr[49]);
 
 	return (0);
 
