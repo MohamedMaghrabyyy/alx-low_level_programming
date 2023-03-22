1f9cd3643a16369cc0493352dee66e6e00468512
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-22 16:17:13 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	unsigned long int arr[98] = {0};
+	unsigned int arr[98] = {0};
 	int i;
 
 	arr[0] = 1;
 	arr[1] = 2;
 
-	for (i = 2; i < 98; i++)
+	for (i = 2; i <= 97; i++)
 	{
 		arr[i] = arr[i - 1] + arr[i - 2];
 	}
