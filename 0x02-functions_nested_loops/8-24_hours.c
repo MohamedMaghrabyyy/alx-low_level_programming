00990828c4934ae58478fa1e62f3a032c3bd7b8a
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-21 17:04:14 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,32 +5,45 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
+	int k;
+	int l;
 
-	for (i = 0;i <= 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		if (i < 10)
+		for (j = 0; j <= 59; j++)
 		{
-			_putchar(0 + '0');
-			_putchar(i + '0');
-			_putchar(':');
-		}
-		else
-		{
-			_putchar(i + '0');
-			_putchar(':');
-		}
-		for (j = 0;j <= 59; j++)
-		{
+			if (i < 10)
+			{
+				_putchar(0 + '0');
+				_putchar(i + '0');
+				_putchar(':');
+			}
+			else
+			{
+				k = i % 10;
+				l = i / 10;
+				_putchar(l + '0');
+				_putchar(k + '0');
+				_putchar(':');
+			}
 			if (j < 10)
 			{
 				_putchar(0 + '0');
 				_putchar(j + '0');
+				_putchar('\n');
+
 			}
 			else
 			{
-				_putchar(j + '0');
-			}
+				k = i % 10;
+				l = j / 10;
+				_putchar(l + '0');
+				_putchar(k + '0');
+				_putchar('\n');
 			
+			}
+
+				
 		}
 
 	}
