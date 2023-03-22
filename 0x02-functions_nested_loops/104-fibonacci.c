375abcf857a29a446f2540e252dc411233401b2f
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-22 16:03:25 -0700

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long arr[98] = {0};
+	int i;
+
+	arr[0] = 1;
+	arr[1] = 2;
+
+	for (i = 2; i < 98; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	for(i = 0; i <= 97; i++)
+	{
+		printf("%ld, ", arr[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
