d7b5da3b2b62f666b06329476c57533c2da878b2
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-23 04:49:56 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - check if entered integer is ascii for an uppercase letter
+ * @c: number to be checked
+ *
+ * Return: 1 if uppercase
+ * otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
