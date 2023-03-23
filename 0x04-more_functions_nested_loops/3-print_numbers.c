427e6701f59728ad00e3ebed7d790986596f21f5
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-23 05:01:31 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i<= 57; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
