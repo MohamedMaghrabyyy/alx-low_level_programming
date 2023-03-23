7680ebe2f247643701c65a52683a0f0165861c49
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-23 08:18:12 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-
+	
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
@@ -29,6 +29,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
+	
 	return (0);
 }
