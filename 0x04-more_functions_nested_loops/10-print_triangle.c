b37a35a99105b6deaf3f89f7c529ed5429b02d28
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-23 11:46:39 -0700

    Task11

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: length of base and height
+ *
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j >= 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
