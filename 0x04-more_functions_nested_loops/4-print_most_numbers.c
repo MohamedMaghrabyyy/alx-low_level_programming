e27f7635c2b17daf318d1c7da4d456354149f4af
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-23 05:09:39 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 & 4
+ * @i: integer to be checked
+ *
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 && i == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
