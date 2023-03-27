d907136975d2b889cb572ee7e29773576528b20e
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-27 14:50:22 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - copies string in array src into buffer dest
+ * @dest: buffer to copy into
+ * @src: string to copy
+ *
+ * Return: copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count =0;
+	int i;
+
+	while (1)
+	{
+		if (src[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
