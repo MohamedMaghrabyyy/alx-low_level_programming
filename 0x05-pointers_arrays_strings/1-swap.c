5bedffd334b71f3377001289fee691ed64d3802c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-27 12:23:16 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * swap_int - swap two integer pointers
+ * @a: pointer to first integer
+ * @b: pointer to second integer
+ *
+ * Return: nothing
+ */
+void swap_int( int *a, int *b)
+{
+	int temp = *b;
+	*b = *a;
+	*a = temp;
+}
