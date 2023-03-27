40c9ec3ef4a958898d16f861ff1a14f714ac6eeb
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-27 12:32:21 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _strlen - returns length of a string
+ * @s: String required to get its length
+ *
+ * Return: number of characters in a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int count = 0;
+	
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
