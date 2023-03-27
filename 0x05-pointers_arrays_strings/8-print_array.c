c834671b6d638626899b02b4c79758e6a0efa039
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-03-27 14:32:02 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - print first n elements of an array
+ * @a: passed array
+ * @n: length of passed array
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	int l = strlen(a) - 1;
+
+	for (i = 0; i <= n-2; i++)
+	{
+		printf("%d, ", a[i]);
+
+	}
+	putchar(a[n-1]);
+	putchar('\n');
+}
