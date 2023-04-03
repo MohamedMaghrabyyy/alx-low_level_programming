fc2a4091d15724c3ecc6d6f4c1bbc5d4f530f770
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-03 09:00:25 -0700

    Task 0

--- 0x07-pointers_arrays_strings/memset.c
+++ 0x07-pointers_arrays_strings/memset.c
@@ -13,10 +13,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
+	char *start = s;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		start[i] = b;
 	}
-	return (s);
+	return (start);
 }
