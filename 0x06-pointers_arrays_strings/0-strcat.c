c0e722ad7cd025fd037e03e6a8bd4fad70293eb5
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-03 07:54:26 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * *_strcat - appends one string to another
+ * @dest - string to be extended
+ * @src - string to be appended to the other
+ *
+ * Return: final appended string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = strlen(dest);
+	int j = strlen(src);
+	int k = (i + j);
+	int n;
+	int m =0;
+
+	for (n = i; n < k; n++)
+	{
+		dest[n] = src[m];
+		m += 1;
+	}
+	return (dest);
+}
