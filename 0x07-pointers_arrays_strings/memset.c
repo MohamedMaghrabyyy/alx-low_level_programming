0fb18e08d51f9639ef9cab878007f569154b13ec
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-03 08:52:52 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * *_memset - replaces first n places with char b
+ * @s: array to be editied
+ * @b: char to be inserted to first n places
+ * @n: first n places to insert in
+ *
+ * Return: modified array
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i< n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
