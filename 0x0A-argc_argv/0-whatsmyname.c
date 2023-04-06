9c6db49a714dd23cd530858031f121c57dad182e
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-06 16:52:15 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @*argv: array of pointers to arguments 
+ *
+ * Return: nothing
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s", argv[0]);
+	putchar('\n');
+
+	return (0);
+}
