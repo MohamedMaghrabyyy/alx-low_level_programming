9986bd0518266c5d42c569856f6b676f27405e94
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-06 17:15:16 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: array of pointers pointing to each argument
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	putchar('\n');
+
+	return (0);
+}
