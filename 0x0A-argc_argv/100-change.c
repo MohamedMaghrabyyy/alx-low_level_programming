faff1bd37f7ce6fcb80d15ea0bc8397d38ecf2db
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-10 16:22:22 -0700

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,67 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-/** 
- * main - entry point
- * @argc: number of args
- * @argv: array of pointers to args
- *
- * Return: nothing
+/**
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
-
 int main(int argc, char *argv[])
 {
-	int test25 = 0;
-	int test10 = 0;
-	int test5 = 0;
-	int test2 = 0;
-	int test1 = 0;
-	int k = atoi(argv[1]);
-	int flag =0;
+	int cents, ncoins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		flag = 1;
-	}
-	else
-	{
-		if (k <= 0)
-		{
-			printf("%d\n", 0);
-			return(0);
-		}
-		while (k >= 25)
-		{
-			k -= 25;
-			test25++;
-		}
-		while (k >= 10)
-		{
-			k -= 10;
-			test10++;
-		}
-		while (k >= 5)
-		{
-			k -= 5;
-			test5++;
-		}
-		while (k >= 2)
-		{
-			k -= 2;
-			test2++;
-		}
-		while (k >= 1)
-		{
-			k -= 1;
-			test1++;
-		}
-		printf("%d\n", test25 + test10 + test5 + test2 + test1);
-
-	}
-	if (flag == 1)
-	{
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
 	return (0);
 }
