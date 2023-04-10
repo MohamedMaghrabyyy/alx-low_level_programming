57f72bd1813cb8832490ea477a47f620f8406da9
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-10 16:09:57 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,7 @@
 /**
  * main - entry point
  * @argc: number of arguments
- * @argc: array of pointers to arguments
+ * @argv: array of pointers to arguments
  *
  * Return: 0 (terminates)
  */
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%ld\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
