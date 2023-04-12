644d8532d0f8dcda35da88ff84f1a4561c92d3ee
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-11 17:48:20 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+/**
+ * *_strdup - copies content of an array into a new array
+ * @str: array to copy from
+ *
+ * Return: new array
+ */
+char *_strdup(char *str)
+{
+	int size = strlen(str);
+	char *arr;
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(char) * size);
+	while (i < size)
+	{
+		*(arr + i) = str[i];
+		i++;
+	}
+	return (arr);
+}
