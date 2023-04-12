4385d3af04285e2fbc8a89781bc92615ea5cdf49
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-12 15:24:07 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * factorial - calculates factorial of an integer using recursion
+ * @n: number to get its factorial
+ *
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1)'
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
