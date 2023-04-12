068dec0bd4c5902266e465dd337754cf5bf23f4d
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-11 17:33:41 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	int i = 0;
 	
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	arr = malloc(sizeof(char) * size);
-	*arr = c;
+	while (i < size)
+	{
+		*arr = c;
+		i++;
+	}
 	return (arr);
 }
