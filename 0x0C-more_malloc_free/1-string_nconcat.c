d67a2a18896084cff2f384d26f295b577ee1be4c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-13 15:38:12 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,60 +1,43 @@
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
- * *string_nconcat - concatenate s1 and first n letters of s2
- * @s1: first string to fully return
- * @s2: second string to concat first n letters to the first string
- * @n: number of letters from s2 to concat to s1
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * Return: Concatenated string 
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int a = strlen(s1);
-	unsigned int b = strlen(s2);
-	char *ptr;
-	int i = 0;
-	int j = 0;
-	
+	char *concat;
+	unsigned int len = n, index;
+
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	ptr = malloc(sizeof(char) * (a + n + 1));
-	if (ptr == NULL)
-	{
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	}
-	if (b < n)
-	{
-		while (i < a)
-		{
-			*(ptr + i) = s1[i];
-			i++;
-		}
-		while (j < b)
-		{
-			*(ptr + i + j) = s2[j];
-			j++;
-		}
-		*(ptr + i + j + 1) = '\0';
-		return (ptr);
-	}
-	while (i < a)
-	{
-		*(ptr + i) = s1[i];
-		i++;
-	}
-	while (j < n)
-	{
-		*(ptr + i + j) = s2[j];
-		j++;
-	}
-	*(ptr + i + j + 1) = '\0';
-	return (ptr);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
