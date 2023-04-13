d6649a06b7ff01b5547a6e3bdb32603e7a40a9df
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-13 14:55:54 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,10 +6,9 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *ptr = malloc(b);
 
-	ptr = malloc(b);
-	if (ptr == NULL)
+	if (a == NULL)
 		exit(98);
-	return (ptr);
+	return (a);
 }
