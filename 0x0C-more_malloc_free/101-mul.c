cda7ccc7d33c2ea21133f664eaaf4c9381ff07bd
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-13 17:05:15 -0700

    Task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: numebr of passed arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	long int num1 , num2;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!atoi(argv[1]) || !atoi(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%ld\n", num1  * num2);
+	return (0);
+}
