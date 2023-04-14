399c5b2f55704f125af904d101fae8b87473d718
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-13 17:18:14 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h>
 /**
  * main - Entry point
  * @argc: numebr of passed arguments
@@ -7,24 +8,26 @@
  *
  * Return: (0) success
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	long double num1;
-	long double num2;
-
-	if (argc != 3)
+	if (argc != 3) 
 	{
-		printf("Error\n");
-		exit(98);
+		printf("Usage: %s <arg1> <arg2>\n", argv[0]);
+		return 1;
 	}
-	if (!atoi(argv[1]) || !atoi(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	printf("%Lf\n", num1  * num2);
-	return (0);
+	mpz_t arg1, arg2, result;
+	mpz_init(arg1);
+	mpz_init(arg2);
+	mpz_init(result);
+	mpz_set_str(arg1, argv[1], 10);
+	mpz_set_str(arg2, argv[2], 10);
+	mpz_mul(result, arg1, arg2);
+	
+	gmp_printf("%Zd * %Zd = %Zd\n", arg1, arg2, result);
+	
+	mpz_clear(arg1);
+	mpz_clear(arg2);
+	mpz_clear(result);
+	
+	return 0;
 }
