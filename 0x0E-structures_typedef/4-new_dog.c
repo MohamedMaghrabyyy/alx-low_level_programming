e6b476f54d26f540e8cf3784112aa7170926f577
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-17 11:32:37 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <string.h>
 #include <stdlib.h>
 /**
  * *new_dog - creates a new instance of a struct
@@ -10,11 +11,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
+
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
