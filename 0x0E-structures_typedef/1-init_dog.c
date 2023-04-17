44a0157f1b1c657ae09e128948c90128e1ea318c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-17 10:35:28 -0700

    Task 0

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 #include <stdlib.h>
 /**
  * init_dog - passes values to a struct
@@ -9,9 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
 	d->age;
 	d->name;
 	d->owner;
