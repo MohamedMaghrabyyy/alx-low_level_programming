a8d96826bd7f04f22cbb3bddeefb50e507c6f87c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-17 10:41:12 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,15 @@
  * @owner: owner of dog
  *
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dogg;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
