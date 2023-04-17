c9c844fc4eac24b0052c665d121d9f58be40bc8c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-17 11:42:04 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
 
-	new_dog = calloc(1, sizeof(struct dog));
+	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
+	new_dog->name = malloc((strlen(name) + 1) * sizeof(char));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 	strcpy(new_dog->name, name);
 	new_dog->age = age;
+	new_dog->owner = malloc((strlen(owner) + 1) * sizeof(char));
 	strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
