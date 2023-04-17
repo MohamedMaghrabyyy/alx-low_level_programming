8d1eca4377299578c011732374661944341f4498
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-17 08:33:53 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - creates a struct with dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
