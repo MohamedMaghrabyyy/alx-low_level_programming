1da6540429b37f46d4d44d7a1f4c9f254971042f
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-17 08:53:18 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - passes values to a struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age;
+	d->name;
+	d->owner;
+}
