27ae7591c6cfa43b6f21a6cf455586e1b0951338
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-20 14:56:25 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,15 +18,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d%s", va_arg(ap, int), separator);
 		}
-		printf("%d", va_arg(ap, int));
+		printf("%d\n", va_arg(ap, int));
 		va_end(ap);
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n - 1; i++)
 		{
 			printf("%d", va_arg(ap, int));
 		}
+		printf("%d\n", va_arg(ap ,int));
 		va_end(ap);
 	}
 }
