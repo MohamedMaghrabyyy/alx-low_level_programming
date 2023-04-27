e775137cd5aeb2e0e75f2ebb7db9753f29e5b7b0
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-27 15:16:25 -0700

    Task 4

---
+++ 4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - deletes a linked list from memory
+ * @head: pointer to first node in linked list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (current != NULL)
+	{
+		temp = current;
+		free(temp);
+		current = current->next;
+	}
+	head = NULL;
+}
