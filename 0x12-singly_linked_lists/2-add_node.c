468ec169ffa9e0bcdf07e17b4b37ffe08122ac9c
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-27 11:05:24 -0700

    Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of the list
+ * @head: head pointer of the linked list
+ * @str: pointer to the node to be inserted
+ *
+ * Return: address of the added node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr->str);
+		return (NULL);
+	}
+	ptr->len = strlen(str);
+	if (head == NULL)
+	{
+		*head = ptr;
+	}
+	else
+	{
+		ptr->next = *head;
+		*head = ptr;
+	}
+	return (ptr);
+}
