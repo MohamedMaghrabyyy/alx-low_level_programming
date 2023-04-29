0d98d91258c940255f22e4d7da668396aa9f6e64
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-29 16:09:27 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (*head == NULL)
 		return (NULL);
-	
+
 	while (counter != NULL)
 	{
 		i++;
@@ -26,6 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
+		new->next = *head;
 		*head = new;
 		new->n = n;
 		return (new);
