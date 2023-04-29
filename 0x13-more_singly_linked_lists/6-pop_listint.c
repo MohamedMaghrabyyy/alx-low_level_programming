40f357c1178b21687adb4129ddb6f0c06812e445
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-29 12:56:00 -0700

    Fixed issue in Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,10 +7,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *ptr = *head;
+	int n;
 
 	if (*head == NULL)
 		return (0);
+	n = ptr->n;
 	*head = ptr->next;
 	free(ptr);
-	return (ptr->n);
+	return (n);
 }
