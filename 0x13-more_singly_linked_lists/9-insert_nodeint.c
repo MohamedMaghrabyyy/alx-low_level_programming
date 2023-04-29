bb930217e6af2baba442f12e865f108cf7fb1a19
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-04-29 13:27:58 -0700

    Fixed issue in Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = ptr->next;
 			ptr->next = new;
+			new->n = n;
 			return (new);
 		}
 		ptr = ptr->next;
