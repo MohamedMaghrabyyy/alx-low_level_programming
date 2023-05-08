bf131f8d29a8d124502a5707b4d68f5515c0143a
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-05-08 11:42:46 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file & prints it to posix output
+ * read_textfile - reads a textfile and prints it to posix output
  * @filename: name of file to read from
- * @letters: number of letters in the file
- * Return: number of letters the function could read and print
- */
+ * @letters: number of letters in file
+ * Return: number of letters in file
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
@@ -15,6 +15,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		return (0);
-	printf("%s", fgets(ch, letters, fp));
+	int count = fread(&ch, sizeof(char), letters, fp);
+	printf("%s", ch);
 	fclose(fp);
+	return (count);
 }
