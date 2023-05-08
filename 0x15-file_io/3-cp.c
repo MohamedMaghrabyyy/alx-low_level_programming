ae6481c6fe47f8cc30346aee0a0709f1c1339fdf
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-05-08 14:21:09 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,53 +8,51 @@
  */
 int main(int argc, char **argv)
 {
-    int fd1, fd2;
-    int r_1st;
-    int w_2nd;
-    char *string;
-    int c_test;
+	int fd1, fd2;
+	int r_1st;
+	int w_2nd;
+	char *string;
+	int c_test;
 
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: cp %s %s\n", argv[1], argv[2]);
-        exit(97);
-    }
-    fd1 = open(argv[1], O_RDONLY);
-    if (fd1 == -1)
-    {
-        fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    string = malloc(sizeof(char) * fd1);
-    r_1st = read(fd1, string, 1024);
-    
-    if (r_1st == -1)
-    {
-        fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: cp %s %s\n", argv[1], argv[2]);
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	string = malloc(sizeof(char) * fd1);
+	r_1st = read(fd1, string, 1024);
 
-    fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-    w_2nd = write(fd2, string, r_1st);
-
-    if (w_2nd == -1)
-    {
-        fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-    c_test = close(fd1);
-    if (c_test == -1)
-    {
-        fprintf(stderr, "Error: Can't close fd %s", argv[1]);
-        exit(100);
-    }
-    c_test = close(fd2);
-    if (c_test == -1)
-    {
-        fprintf(stderr, "Error: Can't close fd %s", argv[2]);
-        exit(100);
-    }
-    free(string);
-    return (0);
+	if (r_1st == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	w_2nd = write(fd2, string, r_1st);
+	
+	if (w_2nd == -1)
+	{
+		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	c_test = close(fd1);
+	if (c_test == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %s", argv[1]);
+		exit(100);
+	}
+	c_test = close(fd2);
+	if (c_test == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %s", argv[2]);
+		exit(100);
+	}
+	free(string);
+	return (0);
 }
