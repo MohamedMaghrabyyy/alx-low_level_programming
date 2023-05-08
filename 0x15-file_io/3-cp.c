fd2b34a3a7a862c75487dcdd1ff09f27bb48bbcc
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-05-08 14:41:54 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char **argv)
 {
-	int fd1, fd2, r_1st, w_2nd , c_test , c_test2;
+	int fd1, fd2, r_1st, w_2nd, c_test, c_test2;
 	char ch[1024];
 
 	if (argc != 3)
@@ -23,7 +23,8 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	w_2nd = write(fd2, ch, r_1st);
 	if (w_2nd == -1 || fd2 == -1)
 	{
