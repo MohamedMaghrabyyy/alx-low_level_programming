f3aab0b9593a81ae920bf3385b80fb288c931821
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-05-08 13:14:14 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,25 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len = strlen(text_content);
+	ssize_t len;
+	int num;
 
 	if (filename == NULL)
-	{
-		fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-		if (fd == (-1))
-			return (-1);
-		write(fd, text_content, len);
-		return (1);
-	}
-	else
-	{
-		fd = open(filename, O_WRONLY | O_TRUNC);
-		if (fd == (-1))
-			return (-1);
-		write(fd, text_content, len);
-		return (1);
-	}
+		return (-1);
+
+	fd = open(filename,  O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == (-1))
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	len = strlen(text_content);
+
+	num = write(fd, text_content, len);
+
+	if (num == (-1))
+		return (-1);
+	close(fd);
+	return (1);
 }
