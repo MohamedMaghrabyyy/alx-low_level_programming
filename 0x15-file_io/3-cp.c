cca3a24896ab5ad37b783d6ea28977b0cd666a2e
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-05-08 14:28:45 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,16 +3,13 @@
 /**
  * main - Entry point
  * @argc: number of args
- * @argc: pointer to array of args
+ * @argv: pointer to array of args
  * Return: 0 (terminates)
  */
 int main(int argc, char **argv)
 {
-	int fd1, fd2;
-	int r_1st;
-	int w_2nd;
+	int fd1, fd2, r_1st, w_2nd, c_test, c_test2;
 	char *string;
-	int c_test;
 
 	if (argc != 3)
 	{
@@ -20,35 +17,24 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	string = malloc(sizeof(char) * fd1);
 	r_1st = read(fd1, string, 1024);
-
-	if (r_1st == -1)
+	if (r_1st == -1 || fd1 == -1)
 	{
 		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	w_2nd = write(fd2, string, r_1st);
-	
-	if (w_2nd == -1)
+	if (w_2nd == -1 || fd2 == -1)
 	{
 		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	c_test = close(fd1);
-	if (c_test == -1)
-	{
-		fprintf(stderr, "Error: Can't close fd %s", argv[1]);
-		exit(100);
-	}
-	c_test = close(fd2);
-	if (c_test == -1)
+	c_test2 = close(fd2);
+	if (c_test == -1 || c_test2 == -1)
 	{
 		fprintf(stderr, "Error: Can't close fd %s", argv[2]);
 		exit(100);
