db2c9f1a0d388e199d1d1ea36795cf8dd64656af
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-05-19 12:45:06 -0700

    Task 3 done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strcmp - compares to strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 (if s1 & s2 are the same) 
+ * postive number (s1 is longer)
+ * negative number (s2 is longer)
+ */ 
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, k, bigger;
+	int flag = 0;
+
+	i = strlen(s1);
+	j = strlen(s2);
+
+	if (i > j)
+	{
+		bigger = i - j;
+		return (bigger);
+	}
+	else if (j > i)
+	{
+		bigger = i - j;
+		return (bigger);
+	}
+	else
+	{
+		for (k = 0; k < i; k++)
+		{
+			if (s1[k] == s2[k])
+				continue;
+			else
+			{
+				bigger = s1[k] - s2[k];
+				flag = 1;
+				break;
+			}
+		}
+	}
+	if (!flag)
+		return (0);
+	else
+		return (bigger);
+}
