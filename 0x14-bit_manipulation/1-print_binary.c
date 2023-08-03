ad099c0aefd88143a370389e5c1a99cb26d78c77
MohamedMaghrabyyy <mohamed.maghrabyyy@icloud.com>
2023-08-03 06:27:11 -0700

    tasks done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_binary - prints binary representation of a number
+ * @n: passed integer
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+	int i;
+	int x = 0;
+	unsigned long int digit;
+	unsigned long int array[32];
+
+	if (n == 0)
+		putchar(48);
+	mask = 00000000000000000000000000000001;
+	for (i = 31; i >= 0; i--)
+	{
+		digit = mask & n;
+		array[i] = digit;
+		n = n >> 1;
+	}
+	while (x <= 31)
+	{
+		if (array[x] == 0)
+		{
+			x++;
+			continue;
+		}
+		else
+			break;
+	}
+	while (x <= 31)
+	{
+		printf("%lu", array[x]);
+		x++;
+	}
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - sets the value a bit
+ * @n: passed pointer to an int
+ * @index: index of the element to set
+ * Return:
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	mask = 00000000000000000000000000000001;
+	mask = mask << index;
+	*n = *n | mask;
+
+	return (1);
+}
